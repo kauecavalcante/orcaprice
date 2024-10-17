ec33077c3c39917aa4a8466e1797e14453f890a9
Kauê Cavalcante <Kauecavalcante4@gmail.com>
2024-10-17 14:39:35 -0300

    Estilização e criação do card dos projetos

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,18 +1,37 @@
 import { useLocation } from "react-router-dom"
 
+
 import Message from "../layout/Message"
 import Container from '../layout/Container'
 import LinkButton from "../layout/LinkButton"
+import ProjectCard from "../project/ProjectCard"
 
 import styles from './Projects.module.css'
+import { useState, useEffect } from "react"
 
 function Projects() {
+    const [projects, setProjects] = useState([]) // criando um state para salvar os projetos. começando em um array vazio
 
     const location = useLocation()
     let message = ''
     if(location.state){
         message = location.state.message
     }
+
+    useEffect(() => {
+
+        fetch('http://localhost:3001/projects', {
+            method: 'GET', 
+            headers: { 
+                'Content-Type': 'application/json',
+            },
+        }).then(resp => resp.json())
+        .then(data => {
+            setProjects(data)
+        })
+        .catch((err) => console.log(err))
+
+    }, [])
 
     return (
         <div className={styles.project_container}>
@@ -22,9 +41,13 @@ function Projects() {
             </div>
             {message && <Message type='success' msg={message} />}
             <Container customClass='start'>
-                <p>Projetos...</p>
+                {projects.length > 0 && 
+                  projects.map((project) => 
+                    <ProjectCard id={project.id} name={project.name} budget={project.budget} category={project.category.name}  key={project.id} />
+                  )
+                }
             </Container>
-        </div>
+        </div> 
     )
 }
 
