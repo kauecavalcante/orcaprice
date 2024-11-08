c1d952dfbb00c002b256539de14178efcf71ece6
Kauê Cavalcante <Kauecavalcante4@gmail.com>
2024-11-07 21:27:30 -0300

    finalização do projeto
    Co-Authored-By: JoaoPedro-09 <178346626+JoaoPedro-09@users.noreply.github.com>
    Co-Authored-By: Maria Julia Feitosa <162568486+marjufeitosa@users.noreply.github.com>

--- src/components/pages/Company.js
+++ src/components/pages/Company.js
@@ -1,5 +1,12 @@
+import styles from './Contact.module.css'
+
 function Company() {
-    return <h1>Company</h1>
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}> Empresa </h1>
+            <p className={styles.title}>Esse projeto foi desenvolvido em <span className={styles.bold}> Reactjs </span>usando o amazenado local para armazenar seus projetos e serviços.</p>
+        </div>
+    )
 }
 
 export default Company
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,5 +1,29 @@
+import {Link} from 'react-router-dom'
+
+import styles from './Contact.module.css'
+
 function Contact() {
-    return <h1>Contact</h1>
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Contato</h1>
+            <p className={styles.title}>Esse projeto foi desenvolvido por:</p>
+            <ul  className={styles.lista}>
+                <li>
+                 <Link to="https://github.com/kauecavalcante" target='_blank'>Kauê Cavalante</Link>
+                </li>
+                <li>
+                <Link to="https://github.com/marjufeitosa" target='_blank'>Maju Feitosa</Link>
+                </li>
+                <li>
+                <Link to="https://github.com/pedroteixeira02" target='_blank'>Pedro Teixeira</Link>
+                </li>
+                <li>
+                <Link to="https://github.com/JoaoPedro-09" target='_blank'>João Pedro</Link>
+                 </li>
+            
+            </ul>
+        </div>
+    )
 }
 
 export default Contact
