64b8ac7fcb05a9ee05d9dc4baf490586a1c3fef2
Kauê Cavalcante <Kauecavalcante4@gmail.com>
2024-11-09 07:01:57 -0300

    update do company

--- src/components/pages/Company.js
+++ src/components/pages/Company.js
@@ -4,7 +4,7 @@ function Company() {
     return (
         <div className={styles.container}>
             <h1 className={styles.title}> Empresa </h1>
-            <p className={styles.title}>Esse projeto foi desenvolvido em <span className={styles.bold}> Reactjs </span>usando o amazenado local para armazenar seus projetos e serviços.</p>
+            <p className={styles.title}>Esse projeto foi desenvolvido em <span className={styles.bold}> Reactjs </span>usando o amazenado e a autenticação do firebase para armazenar seus projetos e serviços.</p>
         </div>
     )
 }
